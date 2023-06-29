b19a6ebff5b593fac4135969d907107bf14da61b
BarrierHT <facuro1000@outlook.com>
2023-06-29 16:37:17 -0300

    Routing modified

--- gestor-videos-backend/src/controllers/auth.js
+++ gestor-videos-backend/src/controllers/auth.js
@@ -1,41 +1,78 @@
-import userSchema from "../models/Usuario.js";
+import userSchema from '../models/Usuario.js';
+import { errorHandler } from '../utils/errorHandler.js';
+import bcrypt from 'bcryptjs';
 
 //Controller para gestionar la autenticacion usuarios normales
 
 const postLogin = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
+	const { email, password } = req.body;
 
-    res.json(`Usuario: ${username} | Contraseña: ${password}`);
-  } catch (err) {
-    res.status(500).json({ message: "Ocurrio un error, pruebe mas tarde" });
-    console.log(err);
-  }
+	console.log(email, password);
+
+	let loadedUser = null;
+
+	return userSchema
+		.findOne({ email })
+		.then(user => {
+			if (!user)
+				throw errorHandler(
+					'A user with this email was not found',
+					401,
+					{}
+				);
+			loadedUser = user;
+			//ToDo return bcrypt.compare(password, user.password);
+			return password === user.password;
+		})
+		.then(hasMatch => {
+			if (!hasMatch)
+				throw errorHandler(
+					'A user with this password was not found',
+					401,
+					{}
+				);
+			const token = jwt.sign(
+				{ email, userId: loadedUser._id.toString() },
+				process.env.SECRET_JWT_AUTHENTICATION,
+				{ expiresIn: '1h' }
+			);
+
+			return res.status(200).json({
+				message: 'User logged correctly!',
+				token,
+				userId: loadedUser._id.toString(),
+			});
+		})
+		.catch(err => {
+			err.statusCode = 500;
+			next(err);
+			return err;
+		});
 };
 
 const postSignup = async (req, res, next) => {
-  try {
-    const { nombre, password, correo, telefono, username } = req.body;
+	try {
+		const { nombre, password, correo, telefono, username } = req.body;
 
-    const newUser = new userSchema({
-      nombre: nombre,
-      password: password,
-      correo: correo,
-      telefono: telefono,
-      username: username,
-    });
+		const newUser = new userSchema({
+			nombre: nombre,
+			password: password,
+			correo: correo,
+			telefono: telefono,
+			username: username,
+		});
 
-    await newUser.save();
-    res.json(newUser);
-  } catch (err) {
-    res.status(500).json({ message: "Ocurrio un error, pruebe mas tarde" });
-    console.log(err);
-  }
+		await newUser.save();
+		res.json(newUser);
+	} catch (err) {
+		res.status(500).json({ message: 'Ocurrio un error, pruebe mas tarde' });
+		console.log(err);
+	}
 };
 
 const authController = {
-  postLogin,
-  postSignup,
+	postLogin,
+	postSignup,
 };
 
 export default authController;
--- gestor-videos-frontend/src/app/app-routing.module.ts
+++ gestor-videos-frontend/src/app/app-routing.module.ts
@@ -5,10 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 import { ListVideoComponent } from './components/list-video/list-video.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { SearchedVideosComponent } from './components/searched-videos/searched-videos.component';
+import { ShowVideoComponent } from './components/show-video/show-video.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'list-video', component: ListVideoComponent },
+  { path: 'playlist', component: ListVideoComponent },
+  { path: 'results', component: SearchedVideosComponent },
+  { path: 'watch/:videoId', component: ShowVideoComponent },
+
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 
@@ -20,4 +25,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
