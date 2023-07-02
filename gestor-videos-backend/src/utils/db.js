0be15547e8ab45351cdfc0e72cd3627ccb4a3f06
BarrierHT <facuro1000@outlook.com>
2023-07-01 22:56:14 -0300

    database shared

--- gestor-videos-backend/src/utils/db.js
+++ gestor-videos-backend/src/utils/db.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 
 (async () => {
 	try {
-		await mongoose.connect('mongodb://127.0.0.1/gestionvideos', {
+		const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.qmwzndr.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+		console.log(uri);
+		await mongoose.connect(uri, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
+			maxPoolSize: 10,
 		});
 		console.log('MongoDB Connection successful');
 	} catch (err) {
