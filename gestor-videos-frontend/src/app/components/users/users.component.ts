a90d47eff895fae45af1eb020be458375cf0bcb9
Facundo Romero <59918131+BarrierHT@users.noreply.github.com>
2023-07-09 20:04:04 -0300

    Routes protected.changes in videos and users, and validation forms
    All routes protected in front. and some changes in videos and users, and validation in all forms

--- gestor-videos-frontend/src/app/components/users/users.component.ts
+++ gestor-videos-frontend/src/app/components/users/users.component.ts
@@ -1,15 +1,16 @@
 import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import * as printJS from 'print-js';
 import { Subject } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit, OnDestroy {
-
   users: any = [];
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject<any>();
@@ -18,13 +19,14 @@ export class UsersComponent implements OnInit, OnDestroy {
 
   constructor(
     private userService: UserService,
-    private cdr: ChangeDetectorRef
-  ) { }
+    private cdr: ChangeDetectorRef,
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.dtOptions = {
       pagingType: 'full_pages',
-      pageLength: 5
+      pageLength: 5,
     };
     this.getUsers();
   }
@@ -34,35 +36,37 @@ export class UsersComponent implements OnInit, OnDestroy {
   }
 
   saveUser() {
-    this.userService.updateUser(this.editingUser._id, this.editingUser).subscribe(
-      res => {
-        console.log(res);
-        alert('Usuario actualizado correctamente');
-        this.getUsers();
-        this.editingUser = null;
-        this.closeEdit();
-      },
-      err => {
-        console.log(err);
-        alert('Error al actualizar el usuario');
-      }
-    );
+    this.userService
+      .updateUser(this.editingUser._id, this.editingUser)
+      .subscribe(
+        (res) => {
+          console.log(res);
+          alert('Usuario actualizado correctamente');
+          this.getUsers();
+          this.editingUser = null;
+          this.closeEdit();
+        },
+        (err) => {
+          console.log(err);
+          alert('Error al actualizar el usuario');
+        }
+      );
   }
 
   deleteUser(idUser: string) {
-    this.userService.deleteUser(idUser).subscribe(res => {
+    this.userService.deleteUser(idUser).subscribe((res) => {
       try {
         console.log(res);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    })
+    });
     alert('Usuario eliminado correctamente');
     this.getUsers();
   }
 
   getUsers() {
-    this.userService.getUsers().subscribe(res => {
+    this.userService.getUsers().subscribe((res) => {
       try {
         console.log(res);
         this.users = res;
@@ -81,10 +85,37 @@ export class UsersComponent implements OnInit, OnDestroy {
     console.log(this.editingUser);
   }
 
-
   closeEdit() {
     this.isEditModalOpen = false;
     this.editingUser = null;
   }
-}
 
+  generatePDF() {
+    let usersPrint: Array<any> = this.procesarListado(this.users);
+    printJS({
+      printable: usersPrint,
+      showModal: true,
+      properties: ['_id', 'name', 'email', 'rol', 'country'],
+      type: 'json',
+      header:
+        '<h3 class="" style="text-align: center;">Todos los Usuarios</h3>',
+      gridStyle: 'border: 2px solid #3971A5;',
+    });
+    this.toastrService.info('Se genero un PDF', 'Generación PDF');
+  }
+
+  procesarListado(users: Array<any>): Array<any> {
+    let userProcess: Array<any> = new Array<any>();
+    users.forEach((user) => {
+      let userTemp = {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        rol: user.role,
+        country: user.country.name,
+      };
+      userProcess.push(userTemp);
+    });
+    return userProcess;
+  }
+}
